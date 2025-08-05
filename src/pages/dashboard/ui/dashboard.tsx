d2cf292bcb819e3914032d89efedc6f1df1e78e0
devshanematte <devshanematte@gmail.com>
2025-08-05 17:45:24 +0500

    update

--- src/pages/dashboard/ui/dashboard.tsx
+++ src/pages/dashboard/ui/dashboard.tsx
@@ -7,6 +7,22 @@ export const DashboardPage = () => {
 	return (
 		<div className={styles.wrapper}>
 			<RecomendationBooks title="Рекомендуемые книги из библиотеки" />
+			<section className={styles.categoriesAndInformation}>
+				<section className={styles.left}>
+					<RecomendationBooks title="Рекомендуемые книги из библиотеки" />
+					<BookCategories title="Категории книг 1" />
+					<BookCategories title="Категории книг 2" />
+				</section>
+				<section className={styles.right}>
+					<BookCategories title="Категории книг 1" />
+					<BookCategories title="Категории книг 2" />
+					<BookCategories title="Категории книг 3" />
+					<BookCategories title="Категории книг 4" />
+					<BookCategories title="Категории книг 4" />
+					<BookCategories title="Категории книг 4" />
+					<BookCategories title="Категории книг 4" />
+				</section>
+			</section>
 			<BookCategories title="Категории книг" />
 		</div>
 	);
