7d35e6db79a4ade25cd5535a0f51ac743db8f3db
devshanematte <devshanematte@gmail.com>
2025-08-05 20:16:18 +0500

    update

--- src/shared/layouts/widget/ui/widget.layout.tsx
+++ src/shared/layouts/widget/ui/widget.layout.tsx
@@ -1,16 +1,22 @@
-import { Typography } from "antd";
+import { Divider, Typography } from "antd";
 import styles from "../styles/widget-layout.module.scss";
 import { FC, PropsWithChildren } from "react";
 
 export const WidgetLayout: FC<
-	PropsWithChildren<{ title: string; component?: React.ReactElement }>
+	PropsWithChildren<{
+		title: string;
+		component?: React.ReactElement;
+	}>
 > = ({ title, children, component }) => {
 	return (
 		<section className={styles.wrapper}>
 			<section className={styles.header}>
-				<Typography.Title level={4}>{title}</Typography.Title>
+				<Typography.Title style={{ marginBottom: 0 }} level={4}>
+					{title}
+				</Typography.Title>
 				{component}
 			</section>
+			<Divider />
 			{children}
 		</section>
 	);
